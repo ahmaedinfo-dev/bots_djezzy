2de44a9a7a6ea6315717d9ed54a958161e8e4482
ahmaedinfo-dev <ahmaedinfoinda@gmail.com>
2026-03-03 08:27:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+import os
 import requests
+from flask import Flask, jsonify
 
-url = "https://apim.djezzy.dz/mobile-api/oauth2/registration"
+app = Flask(__name__)
 
-params = {
-    "msisdn": "213795291083",
-    "client_id": "87pIExRhxBb3_wGsA5eSEfyATloa",
-    "scope": "smsotp"
-}
+@app.route("/")
+def home():
+    return "API is running 🚀"
 
-headers = {
-    "User-Agent": "MobileApp/3.0.0",
-    "Content-Type": "application/json"
-}
+@app.route("/test")
+def test_api():
+    try:
+        url = "https://apim.djezzy.dz/mobile-api/oauth2/registration"
 
-data = {
-    "consent-agreement": [{"marketing-notifications": False}],
-    "is-consent": True
-}
+        params = {
+            "msisdn": "213795291083",
+            "client_id": "87pIExRhxBb3_wGsA5eSEfyATloa",
+            "scope": "smsotp"
+        }
 
-response = requests.post(url, params=params, json=data, headers=headers)
+        headers = {
+            "User-Agent": "MobileApp/3.0.0",
+            "Content-Type": "application/json"
+        }
 
-print(response.status_code)
-print(response.text)
+        data = {
+            "consent-agreement": [{"marketing-notifications": False}],
+            "is-consent": True
+        }
+
+        response = requests.post(url, params=params, json=data, headers=headers)
+
+        return jsonify({
+            "status_code": response.status_code,
+            "response": response.text
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
